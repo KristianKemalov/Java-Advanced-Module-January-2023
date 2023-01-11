74d3dc92a46b9f0d5eb51008e5dedaaa963eefbc
Kristian Kemalov <114097437+KristianKemalov@users.noreply.github.com>
2023-01-11 21:50:57 +0200

    Add excercises
    Add excercises

---
+++ StacksAndQueues/_07_MathPotato.java
@@ -0,0 +1,10 @@
+package StacksAndQueues;
+
+import java.util.Scanner;
+
+public class _07_MathPotato {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
